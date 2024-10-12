8faf93618feec1e3b2a63d3ff926918aa143f88a
yousefselim1 <s-yousef.shawky@zewailcity.edu.eg>
2024-10-12 23:28:18 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        return 'Hello ' + username
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
